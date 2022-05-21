2fd0156b5403a24dc6be4d464890252cbfe80ae5
Murat Çalışkan <muratcengcaliskan@gmail.com>
2022-05-21 19:18:28 +0300

    redux added

---
+++ React/covid19/src/redux/covidSlice/covidSlice.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { findAllByDisplayValue } from "@testing-library/react";
+import axios from "axios";
+
+export const fetchData = createAsyncThunk('covid/getData', async ()=>{
+    const res = await axios(`${process.env.REACT_APP_API_BASE_ENDPOINT}`)
+    return res.data;
+})
+
+ export const covidSlice=createSlice({
+     name:"covid",
+     initialState:{
+        data:[],
+        loading:false,
+        error:""
+     },
+     reducers:{},
+     extraReducers:{
+         [fetchData.pending]:(state,action)=>{
+            state.loading=true;
+         },
+         [fetchData.fulfilled]:(state,action)=>{
+             state.loading=false;
+            state.data=action.payload;
+         },
+         [fetchData.rejected]:(state,action)=>{
+             state.loading=false;
+             state.error=state.error.message;
+         }
+     }
+ })
