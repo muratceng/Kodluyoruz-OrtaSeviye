db731f17bab30310844c23f2cb46ca967f0a7160
Murat Çalışkan <muratcengcaliskan@gmail.com>
2022-06-06 00:31:31 +0300

    images changed with background-image

--- React/turkish-checkers-game/src/Components/Tile/index.js
+++ React/turkish-checkers-game/src/Components/Tile/index.js
@@ -1,9 +1,13 @@
 export default function Tile({number,image}){
 
     if(number%2 ===0){
-       return <div className="black tile"><img src={image}></img></div>
+       return <div className={"black tile"} >
+           {image && <div className="piece" style={{backgroundImage:`url(${image})`}}></div>}
+       </div>
     }else{
-        return <div className="white tile"><img src={image}></img></div>
+        return <div className={"white tile"} >
+        {image && <div className="piece" style={{backgroundImage:`url(${image})`}}></div>}
+        </div>
     }
 }
 
